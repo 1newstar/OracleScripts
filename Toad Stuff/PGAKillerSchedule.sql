8849e1cc1efd3458396d33e2b1b6902faecc8fdd
NormanDunbar <norman@dunbar-it.co.uk>
2017-07-28 11:21:51 +0100

    Cannot have the job name, for the scheuler, the same as the procedure being called. Sigh.

--- Toad Stuff/PGAKillerSchedule.sql
+++ Toad Stuff/PGAKillerSchedule.sql
@@ -2,10 +2,15 @@ set lines 2000 trimspool on pages 2000
 
 spool PGAKillerSchedule.log
 
+begin
+    sys.dbms_scheduler.drop_job(job_name => 'SYS.PGA_SESSION_KILLER', force => true);
+end;
+/    
+
 BEGIN
   SYS.DBMS_SCHEDULER.CREATE_JOB
     (
-       job_name        => 'SYS.PGASESSIONKILLER'
+       job_name        => 'SYS.PGA_SESSION_KILLER'
       ,start_date      => TO_TIMESTAMP_TZ('2017/07/06 15:00:00.000000 +01:00','yyyy/mm/dd hh24:mi:ss.ff tzr')
       ,repeat_interval => 'FREQ=MINUTELY;INTERVAL=30'
       ,end_date        => NULL
@@ -23,37 +28,37 @@ The session''s being killed will be logged to the alert.log and to the
 trace file for the scheduler session.'
     );
   SYS.DBMS_SCHEDULER.SET_ATTRIBUTE
-    ( name      => 'SYS.PGASESSIONKILLER'
+    ( name      => 'SYS.PGA_SESSION_KILLER'
      ,attribute => 'RESTARTABLE'
      ,value     => TRUE);
   SYS.DBMS_SCHEDULER.SET_ATTRIBUTE
-    ( name      => 'SYS.PGASESSIONKILLER'
+    ( name      => 'SYS.PGA_SESSION_KILLER'
      ,attribute => 'LOGGING_LEVEL'
      ,value     => SYS.DBMS_SCHEDULER.LOGGING_FULL);
   SYS.DBMS_SCHEDULER.SET_ATTRIBUTE_NULL
-    ( name      => 'SYS.PGASESSIONKILLER'
+    ( name      => 'SYS.PGA_SESSION_KILLER'
      ,attribute => 'MAX_FAILURES');
   SYS.DBMS_SCHEDULER.SET_ATTRIBUTE_NULL
-    ( name      => 'SYS.PGASESSIONKILLER'
+    ( name      => 'SYS.PGA_SESSION_KILLER'
      ,attribute => 'MAX_RUNS');
   SYS.DBMS_SCHEDULER.SET_ATTRIBUTE
-    ( name      => 'SYS.PGASESSIONKILLER'
+    ( name      => 'SYS.PGA_SESSION_KILLER'
      ,attribute => 'STOP_ON_WINDOW_CLOSE'
      ,value     => FALSE);
   SYS.DBMS_SCHEDULER.SET_ATTRIBUTE
-    ( name      => 'SYS.PGASESSIONKILLER'
+    ( name      => 'SYS.PGA_SESSION_KILLER'
      ,attribute => 'JOB_PRIORITY'
      ,value     => 3);
   SYS.DBMS_SCHEDULER.SET_ATTRIBUTE_NULL
-    ( name      => 'SYS.PGASESSIONKILLER'
+    ( name      => 'SYS.PGA_SESSION_KILLER'
      ,attribute => 'SCHEDULE_LIMIT');
   SYS.DBMS_SCHEDULER.SET_ATTRIBUTE
-    ( name      => 'SYS.PGASESSIONKILLER'
+    ( name      => 'SYS.PGA_SESSION_KILLER'
      ,attribute => 'AUTO_DROP'
      ,value     => FALSE);
 
   SYS.DBMS_SCHEDULER.ENABLE
-    (name       => 'SYS.PGASESSIONKILLER');
+    (name       => 'SYS.PGA_SESSION_KILLER');
 END;
 /
 
